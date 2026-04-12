c524e594976754d338efe71b5cb24db059ff56d0
Nelson Ferreira <nfgferreira@gmail.com>
2026-04-11 22:39:36 -0400

    Changing Arch package compilation

--- arch-compile.sh
+++ arch-compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Compile this way before creating Arch package
+export CGO_LDFLAGS="$LDFLAGS -Wl,-z,relro,-z,now"
 go build -buildmode=pie -ldflags "-linkmode=external -extldflags=$LDFLAGS" bkp.go
 strip bkp
  
